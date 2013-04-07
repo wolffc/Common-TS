ba467155524ea83346160d619fa8575672bc064f
Christian Wolff <chris@connye.com>
2013-04-07 14:27:05 +0200

    Revert "updated documentation"
    This reverts commit 6c3381e2aa1e8054ef9d423786eb97aa9a031767.

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "commonts".
- *
- * Auto generated 07-04-2013 14:22
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
+########################################################################
+# Extension Manager/Repository config file for ext "commonts".
+#
+# Auto generated 05-09-2012 17:08
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Common TS',
 	'description' => 'Library of Typoscript object to get your Site Development a BOOST',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.5.0',
+	'version' => '1.4.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'dependencies' => array(
-			'typo3' => '4.0.0',
+			'typo3' => '4.0.0-6.0.99',
 		),
 		'depends' => array(
 		),
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"d19a";s:12:"ext_icon.gif";s:4:"b303";s:14:"ext_tables.php";s:4:"ed65";s:9:"README.md";s:4:"80d8";s:10:"todo.tasks";s:4:"97a6";s:14:"doc/manual.sxw";s:4:"7baf";s:19:"doc/wizard_form.dat";s:4:"6ebc";s:20:"doc/wizard_form.html";s:4:"36d8";s:21:"res/demopage/demo1.ts";s:4:"40a1";s:21:"res/demopage/demo2.tx";s:4:"99d3";s:21:"res/demopage/demo3.ts";s:4:"3077";s:21:"res/demopage/demo4.ts";s:4:"d119";s:23:"res/demopage/layout.css";s:4:"d41d";s:27:"res/demopage/css/layout.css";s:4:"ed59";s:23:"res/demopage/js/init.js";s:4:"e573";s:38:"res/demopage/js/jquery-htmlBeautify.js";s:4:"3a57";s:25:"static/base/constants.txt";s:4:"aed0";s:21:"static/base/setup.txt";s:4:"aa4a";s:25:"static/demo/constants.txt";s:4:"dde8";s:21:"static/demo/setup.txt";s:4:"eeb9";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"9a1e";s:12:"ext_icon.gif";s:4:"b303";s:14:"ext_tables.php";s:4:"dbe1";s:9:"README.md";s:4:"0fca";s:14:"doc/manual.sxw";s:4:"e556";s:19:"doc/wizard_form.dat";s:4:"6ebc";s:20:"doc/wizard_form.html";s:4:"5db9";s:25:"static/base/constants.txt";s:4:"84da";s:21:"static/base/setup.txt";s:4:"a03d";}',
 	'suggests' => array(
 	),
 );
