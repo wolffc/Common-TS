93c0e54acb9f1c51369a9018e6bcf07f72e1083f
Christian Wolff <chris@connye.com>
2013-05-17 12:58:03 +0200

    RELEASE 1.6.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "commonts".
-#
-# Auto generated 05-09-2012 17:08
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "commonts".
+ *
+ * Auto generated 17-05-2013 12:57
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Common TS',
 	'description' => 'Library of Typoscript object to get your Site Development a BOOST',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.4.1',
+	'version' => '1.6.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"9a1e";s:12:"ext_icon.gif";s:4:"b303";s:14:"ext_tables.php";s:4:"dbe1";s:9:"README.md";s:4:"0fca";s:14:"doc/manual.sxw";s:4:"e556";s:19:"doc/wizard_form.dat";s:4:"6ebc";s:20:"doc/wizard_form.html";s:4:"5db9";s:25:"static/base/constants.txt";s:4:"84da";s:21:"static/base/setup.txt";s:4:"a03d";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"3b17";s:12:"ext_icon.gif";s:4:"b303";s:14:"ext_tables.php";s:4:"6f6e";s:9:"README.md";s:4:"775e";s:10:"todo.tasks";s:4:"7694";s:14:"doc/manual.sxw";s:4:"0fa0";s:19:"doc/wizard_form.dat";s:4:"6ebc";s:20:"doc/wizard_form.html";s:4:"5db9";s:21:"res/demopage/demo1.ts";s:4:"1944";s:21:"res/demopage/demo2.tx";s:4:"1749";s:21:"res/demopage/demo3.ts";s:4:"a7bb";s:21:"res/demopage/demo4.ts";s:4:"ebb5";s:23:"res/demopage/layout.css";s:4:"d41d";s:27:"res/demopage/css/layout.css";s:4:"a7a4";s:23:"res/demopage/js/init.js";s:4:"5561";s:38:"res/demopage/js/jquery-htmlBeautify.js";s:4:"a33a";s:25:"static/base/constants.txt";s:4:"dbe3";s:21:"static/base/setup.txt";s:4:"47c7";s:25:"static/demo/constants.txt";s:4:"eeb0";s:21:"static/demo/setup.txt";s:4:"abc8";}',
 	'suggests' => array(
 	),
 );
