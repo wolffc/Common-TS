91a07e6a2a0f056acecc147ea2e3249b3051715b
Christian Wolff <chris@connye.com>
2013-05-17 12:52:57 +0200

    changed compatibility version to 4.5.0-6.2.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'dependencies' => array(
-			'typo3' => '4.0.0-6.0.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'depends' => array(
 		),
