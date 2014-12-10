158c32eabe71c627b18b11408e06641b6d61dbfd
Christian Wolff <cwolff@aer.de>
2014-12-10 09:21:30 +0100

    Raised Version Compatibility to 6.2.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Library of Typoscript object to get your Site Development a BOOST',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '1.6.0',
+	'version' => '1.7.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'dependencies' => array(
-			'typo3' => '4.5.0-6.1.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'depends' => array(
 		),
